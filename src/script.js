595860b3290343ea4f5cf3824da236a9ae36c6e3
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-26 20:28:55 -0700

    learned about the spot light

--- src/script.js
+++ src/script.js
@@ -41,6 +41,17 @@ rectAreaLight.position.set(-1.5, 0, 1.5);
 rectAreaLight.lookAt(new THREE.Vector3());
 scene.add(rectAreaLight);
 
+const spotlight = new THREE.SpotLight(
+  0x78ff00,
+  0.5,
+  10,
+  Math.PI * 0.1,
+  0.25,
+  1
+);
+spotlight.position.set(0, 2, 3);
+scene.add(spotlight);
+
 /**
  * Objects
  */
